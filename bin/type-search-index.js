abb95053b58a827593164623afee48c75d326686
Noelia <Cristian@DESKTOP-9RQ4IO4>
2022-02-19 18:29:18 +0100

    FINAL COMPLETITO

---
+++ bin/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Circulo_NRT"},{"p":"<Unnamed>","l":"FiguraGeometrica_NRT"},{"p":"<Unnamed>","l":"Rectangulo_NRT"},{"p":"<Unnamed>","l":"Triangulo_NRT"}]
